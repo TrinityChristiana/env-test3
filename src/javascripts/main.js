30a01c15f9e8c5bcf46a198f1da6fcc1f13e3806
Trinity Christiana <trinitycterry@gmail.com>
2021-02-14 19:27:40 -0600

    Update main.js
    Removed extra trailing space

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -6,7 +6,6 @@ import '../styles/main.scss';
 const init = () => {
   document.querySelector('#app').innerHTML = '<h1>HELLO! You are up and running!</h1>';
   console.log('YOU ARE UP AND RUNNING!');
-  
   // USE WITH FIREBASE AUTH
   // checkLoginStatus();
 };
